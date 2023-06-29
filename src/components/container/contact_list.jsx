4cf7cbd61bc0d49a17df86bd194be3f974e3530b
Luciano Garriga <lucianomgarriga@gmail.com>
2023-06-29 00:05:32 -0300

    Contact Component & Contact List Render

--- src/components/container/contact_list.jsx
+++ src/components/container/contact_list.jsx
@@ -1,25 +1,33 @@
-import React, {useState} from 'react'; 
-import ContactItem from '../pure/contact';
-import { Contact } from '../models/contact.class';
+import React, { useEffect, useState } from "react";
+import ContactItem from "../pure/contact";
+import { Contact } from "../models/contact.class";
 
 const ContactList = () => {
+  const defaultContact1 = new Contact("Mariano", "Gomez", false);
+  // Estado inicial con useState
+  const [contact, setContact] = useState([defaultContact1]);
+  // State Loading
+  const [loading, setLoading] = useState(true);
 
-    const defaultContact = new Contact(
-        "Mariano",
-        "Gomez",
-        true
-    )
+  // Control del ciclo de vida del componente
+  useEffect(() => {
+    console.log("Estado del componente se ha modificado");
+    setLoading(false)
+    return () => {
+      console.log("Componente de Lista de Contactos se desmontara");
+    };
+  }, [contact]);
 
-    function changeState(){
-         
-    }
+  const changeConnected = () => {
+    console.log("MARCAR ESTADO DE CONTACTO");
+  };
 
-    return (
-        <div>
-            <h2>Contact list</h2>
-            <ContactItem contact={defaultContact} changeState={changeState}>Lista de Contactos</ContactItem>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h2>Contact list</h2>
+      <ContactItem contact={defaultContact1} />
+    </div>
+  );
+};
 
 export default ContactList;
--- src/components/pure/contact.jsx
+++ src/components/pure/contact.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Contact } from '../models/contact.class';
 
-const ContactItem = ({contact, changeState}) => {
+const ContactItem = ({contact}) => {
+ 
+    // useEffect p/ saber cuando editemos, modifiquemos un contact
+    useEffect(() => {
+        console.log('Contacto creado');
+        return () => {
+            console.log('Contacto eliminado');
+        };
+    }, [contact]);
+ 
     return (
         <div> 
            <h3>Contact name: {contact.name} {contact.surname}</h3> 
-           <h3>Contact state: {contact.connected ? 'CONNECTED' : 'DISCONNECTED'}</h3>
-           <button onClick={changeState}>Change State</button>
+           <h3>This contact is: {contact.connected ? 'CONNECTED' : 'DISCONNECTED'} </h3> 
+           
         </div>
     );
 }
